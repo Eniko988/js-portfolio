cc1d8d3e91223a57ef1d842d72f44cea1bb77a68
Eniko988 <62260134+Eniko988@users.noreply.github.com>
2023-09-20 21:51:21 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let header = document.querySelector("header");
+  let menu = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
+
+  
+  document.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0);
+  });
+
+
+  let darkmode = document.querySelector("#darkmode");
+
+  darkmode.onclick = () => {
+    console.log("ok");
+    if (darkmode.classList.contains("bx-moon")) {
+      darkmode.classList.replace("bx-moon", "bx-sun");
+      document.body.classList.add("active");
+    } else {
+      darkmode.classList.replace("bx-sun", "bx-moon");
+      document.body.classList.remove("active");
+    }
+  };
+
+  menu.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+  });
+});
+/*
+  let darkmode = document.querySelector("#darkmode");
+
+  darkmode.addEventListener("click", () => {
+
+    if (darkmode.classList.contains("bx-moon")) {
+      darkmode.classList.replace("bx-moon", "bx-sun");
+      document.body.classList.add("active");
+
+    } else {
+      darkmode.classList.replace("bx-sun", "bx-moon");
+      document.body.classList.remove("active");
+    }
+  });
+});
+*/
